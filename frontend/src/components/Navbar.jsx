d576861559c8a5f29d890923340f9494ce04efaa
Aman Kumar <aman.kumar.connect@gmail.com>
2025-12-17 22:34:26 +0530

    Add Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    // use tailwind
+    <nav className="flex items-start gap-4 p-4 bg-accent-950">
+      <div className="w-10 h-10 cursor-pointer flex items-center gap-2">
+        <img src="/logo.png" alt="Site Logo" className="rounded-xl"/>
+        <h1 className="text-accent-500 font-bold text-2xl">Forty4</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
