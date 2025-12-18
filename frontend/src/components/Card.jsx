2909ffd233262ce35ecf5bc8c48999ae9716b89f
Aman Kumar <aman.kumar.connect@gmail.com>
2025-12-18 12:58:19 +0530

    Install react-icons

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
 
 const Card = ({ cardTitle, cardImage, cardData, cardChange }) => {
   return (
     <div className="bg-accent-900 p-4 rounded-lg shadow-md">
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-accent-600 font-medium">{cardTitle}</h2>
-        <img src={cardImage} alt={cardTitle} className="w-8 h-8 rounded-4xl" />
+      <div className="flex justify-between items-center mb-4 text-accent-300">
+        <h2 className=" font-medium">{cardTitle}</h2>
+        {cardImage}
       </div>
-      <div className="text-accent-50 text-2xl font-bold">{cardData}</div>
+      <div className="text-accent-50 text-3xl font-bold">{cardData}</div>
       <div className="text-accent-700 mt-2 flex items-center gap-1">
-        <span className={cardChange > 0 ? "text-accent-500 font-medium" : "text-red-500 font-medium"}>
-          {cardChange >= 0 ? "↑+" : "↓"}
+        {cardChange >= 0 ? (
+          <FaArrowTrendUp className="text-accent-500" />
+        ) : (
+          <FaArrowTrendDown className="text-red-500" />
+        )}
+        <span
+          className={
+            cardChange >= 0
+              ? "text-accent-500 font-medium"
+              : "text-red-500 font-medium"
+          }
+        >
           {cardChange}%{" "}
         </span>
         vs last month
--- frontend/src/components/SearchSection.jsx
+++ frontend/src/components/SearchSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { IoFilter } from "react-icons/io5";
+import { IoMdSearch } from "react-icons/io";
 
 const SearchSection = () => {
   return (
     <div className="flex flex-col md:flex-row justify-start items-center p-8 gap-6">
       <div className="bg-accent-900 text-accent-50 border-2 border-accent-700 rounded-2xl flex items-center gap-2 p-2 px-4 w-full md:max-w-md">
-        <img src="search.png" alt="Search icon" className="w-5 h-5" />
+        <IoMdSearch />
         <input
           type="text"
           placeholder="Search users by name, email..."
@@ -12,7 +14,7 @@ const SearchSection = () => {
         />
       </div>
       <div className="flex items-center justify-center gap-2 bg-accent-900 text-accent-50 border-2 border-accent-700 rounded-2xl p-2 px-4 cursor-pointer w-full md:w-fit">
-        <img src="filter.png" alt="Filter icon" className="w-5 h-5" />
+        <IoFilter />
         <span className="">Filter</span>
       </div>
     </div>
