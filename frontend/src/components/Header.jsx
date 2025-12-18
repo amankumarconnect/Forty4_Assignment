53e109a88eedca62ed31572226ccb99149f8d53a
Aman Kumar <aman.kumar.connect@gmail.com>
2025-12-18 16:08:39 +0530

    Update Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,18 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className=" flex justify-between items-center p-8">
-      <div className="flex flex-col gap-1">
-        <h1 className="font-bold text-4xl text-accent-50">User Management Dashboard</h1>
-        <p className="text-accent-600 font-bold">
-          View, edit and manage user access across the platform.
-        </p>
-      </div>
-      <div>
-        <button className="bg-accent-500 text-accent-950 px-4 py-2 rounded-2xl font-medium shadow-md shadow-accent-700 cursor-pointer">
-          + Add New User
-        </button>
-      </div>
+    <div className="flex flex-col gap-1 px-8 py-4">
+      <h1 className="font-bold text-3xl text-accent-50">User Management</h1>
+      <p className="text-accent-600 font-bold">
+        Manage access, view details, and update user information.
+      </p>
     </div>
   );
 };
